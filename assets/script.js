30e0b1b1ec4fce22c97c5a99a2cb9cb5667b4321
AdrianaLMR <trevenlynk038@gmail.com>
2024-03-27 16:51:22 -0300

    Corrigindo erros de validacoes

--- assets/script.js
+++ assets/script.js
@@ -6,95 +6,120 @@ const inputPassword = document.getElementById("signupPassword");
 const inputConfirmPassword = document.getElementById("confirmPassword");
 const submit = document.getElementById("button-submit");
 
+const uppercaseRegex = /[A-Z]/;
+const symbolRegex = /[!@#$%^&*(),.?":{}|<>]/;
+const numberRegex = /[0-9]/;
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    // Verifica se o campo Nome está preenchido e contém apenas letras ou caracteres específicos
-    const nameRegex = /^[a-zA-ZÀ-ÿ\u00f1\u00d1\s']+$/; // Aceita letras, espaços e apóstrofos
-    if (!nameRegex.test(inputName.value.trim())) {
-        alert("Por favor, digite um nome válido.");
-        return;
-    }
+    if (!validarNome()) return;
+    if (!validarEmail()) return;
+    if (!validarIdade()) return;
+    if (!validarSenha()) return;
+    if (!confirmarSenha()) return;
 
-    // Verifica se o campo Nome está preenchido 
-    if (inputName.value === "") {
-        alert("Por favor, digite seu nome");
-        return;
-    }
-
-    //Verifica se o campo  Email está preenchido e se é válido
-    if (inputEmail.value === "") {
-        alert("Por favor, digite seu E-mail");
-        return;
-    }
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(inputEmail.value)) {
-        alert("Insira um email válido");
-        return;
-    }
-
-    //Verifica se o campo Idade está preenchido e se é maior de idade
-    if (inputIdade.value === "") {
-        alert("Por favor, digite sua idade");
-        return;
-    }
-
-    if ((inputIdade.value > 0) && (inputIdade.value > 100)) {
-        alert("Digite uma idade válida");
-        return;
-    }
-
-    //Verifica se o campo senha está preenchido/ Quantia de caracteres/ tipos de caracteres requeridos
-    const uppercaseRegex = /[A-Z]/; // Pelo menos uma letra maiúscula
-    const symbolRegex = /[!@#$%^&*(),.?":{}|<>]/; // Pelo menos um símbolo
-    const numberRegex = /[0-9]/; // Pelo menos um número
-
-    const inputValue = inputPassword.value.trim();
-
-    if (inputValue === "") {
-        alert("Por favor, digite uma senha válida");
-        return;
-    }
-
-    if (!uppercaseRegex.test(inputValue) || !symbolRegex.test(inputValue) || !numberRegex.test(inputValue) || inputValue.length < 8) {
-        let errorMessage = "A senha deve conter:";
-        if (!uppercaseRegex.test(inputValue)) {
-            errorMessage += " uma letra maiúscula,";
-        }
-        if (!symbolRegex.test(inputValue)) {
-            errorMessage += " um símbolo,";
-        }
-        if (!numberRegex.test(inputValue)) {
-            errorMessage += " um número,";
-        }
-        if (inputValue.length < 8) {
-            errorMessage += " no mínimo 8 caracteres.";
-        } else {
-            errorMessage = errorMessage.slice(0, -1) + ".";
-        }
-        alert(errorMessage);
-        return;
-    }
-
-    //Verifica se o campo confirmar senha está preenchido e se é igual ao campo de senha
-    if (inputConfirmPassword.value === "") {
-        alert("Por favor, digite novamente sua senha para confirmar");
-        return;
-    }
-
-    if (inputConfirmPassword.value !== inputPassword.value) {
-        alert("As senhas não são iguais.");
-        return;
-    }
-
-    //se todos os campos estiverem preenchidos, envie o formulário
+    // Se todos os campos estiverem preenchidos corretamente, envie o formulário
     window.location.href = "https://adrianalmr.github.io/Projetos_Cronometro_Temporizador/";
 });
 
+function validarNome() {
+    const nameRegex = /^[a-zA-ZÀ-ÿ\u00f1\u00d1\s']+$/;
 
-//verificar validacao emial html, 
+    if (!nameRegex.test(inputName.value.trim())) {
+        alert("Por favor, digite um nome válido.");
+        return false;
+    }
+
+    return true;
+}
+
+function validarEmail() {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(inputEmail.value)) {
+        alert("Insira um email válido");
+        return false;
+    }
+
+    return true;
+}
+
+function validarIdade() {
+    const input = inputIdade.value.trim();
+    
+    // Verificar se a entrada contém apenas dígitos
+    if (!/^\d+$/.test(input)) {
+        alert("Digite uma idade válida (apenas números)");
+        return false;
+    }
+    
+    // Converter a entrada em um número inteiro
+    const idade = parseInt(input);
+    
+    // Verificar se a entrada é um número válido dentro do intervalo permitido
+    if (isNaN(idade) || idade < 1 || idade > 110) {
+        alert("Digite uma idade válida");
+        return false;
+    }
+
+    return true;
+}
+
+function validarSenha() {
+
+    const inputValue = inputPassword.value.trim();
+
+    if (!uppercaseRegex.test(inputValue) || !symbolRegex.test(inputValue) || !numberRegex.test(inputValue) || inputValue.length < 8) {
+        let errorMessage = "A senha deve conter:";
+        if (!uppercaseRegex.test(inputValue)) errorMessage += " uma letra maiúscula,";
+        if (!symbolRegex.test(inputValue)) errorMessage += " um símbolo,";
+        if (!numberRegex.test(inputValue)) errorMessage += " um número,";
+        if (inputValue.length < 8) errorMessage += " no mínimo 8 caracteres.";
+        else errorMessage = errorMessage.slice(0, -1) + ".";
+        alert(errorMessage);
+        return false;
+    }
+
+    return true;
+}
+
+function confirmarSenha() {
+    // if (inputConfirmPassword.value === "") {
+    //     alert("Por favor, digite novamente sua senha para confirmar");
+    //     return false;
+    // }
+
+    if (inputConfirmPassword.value !== inputPassword.value) {
+        alert("As senhas não são iguais.");
+        return false;
+    }
+
+    return true;
+}
+
+// Função para limpar todos os campos do formulário
+function limparCampos(event) {
+    inputName.value = "";
+    inputEmail.value = "";
+    inputIdade.value = "";
+    inputPassword.value = "";
+    inputConfirmPassword.value = "";
+}
+
+// Adiciona um ouvinte de evento para quando a página é carregada
+window.addEventListener('load', (event) => {
+    // Chama a função para limpar os campos do formulário
+    limparCampos(event);
+});
+ 
+
+//verificacao de password e confirmpassoword  type: password
+//buttos de visualizar senha
+//corrigir css de buttopn submit e form 
 //guardar as informacoes de cadastro, para login posterior
 //opcao de visualizar senha
-// ao submeter formulario limpar campos se reiniciado novamente
 // click enter direciona ao proximo campo input
+
+
+//erro idade, numerios <100> com simbolos sao aceitos como idade / - apenas simbolos em idade nao sao aceitos 
+//
